c8ebf3246df5a7d14faeeb386878b8b43f0f182f
nunchaku89 <72784652+nunchaku89@users.noreply.github.com>
2021-02-09 14:27:22 +0900

    Update model.go

--- model/model.go
+++ model/model.go
@@ -10,3 +10,9 @@ type Person struct {
 	Name  nullable.String	`json:"P_Name"`
 	Email nullable.String	`json:"P_Email"`
 }
+
+// Paging - 
+type Paging struct {
+	Limit nullable.Int				`json:"limit"`
+	Offset nullable.Int				`json:"offset"`
+}
